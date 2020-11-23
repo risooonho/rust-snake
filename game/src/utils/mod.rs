de01922ab067046029051703b704e4044909852b
Justin Hurstwright <jryanhurst@gmail.com>
2020-11-23 09:02:46 -0800

    lint: Fix rust-analysis errors, and dead code warnings

--- game/src/utils/mod.rs
+++ game/src/utils/mod.rs
@@ -9,99 +9,124 @@ pub struct Color(Vec4);
 impl Color {
     pub fn as_u8(&self) -> [u8; 4] {
         [
-            (self.0.x * 255.0).max(0.0).min(255.0) as u8,
-            (self.0.y * 255.0).max(0.0).min(255.0) as u8,
-            (self.0.z * 255.0).max(0.0).min(255.0) as u8,
-            (self.0.w * 255.0).max(0.0).min(255.0) as u8,
+            (self.0.x * 255.0f32).max(0.0).min(255.0) as u8,
+            (self.0.y * 255.0f32).max(0.0).min(255.0) as u8,
+            (self.0.z * 255.0f32).max(0.0).min(255.0) as u8,
+            (self.0.w * 255.0f32).max(0.0).min(255.0) as u8,
         ]
     }
 
+    #[allow(dead_code)]
     pub fn light_gray() -> Color {
         (200, 200, 200, 255).into()
     }
 
+    #[allow(dead_code)]
     pub fn gray() -> Color {
         (130, 130, 130, 255).into()
     }
 
+    #[allow(dead_code)]
     pub fn dark_gray() -> Color {
         (80, 80, 80, 255).into()
     }
+
+    #[allow(dead_code)]
     pub fn yellow() -> Color {
         (253, 249, 0, 255).into()
     }
 
+    #[allow(dead_code)]
     pub fn gold() -> Color {
         (255, 203, 0, 255).into()
     }
 
+    #[allow(dead_code)]
     pub fn pink() -> Color {
         (255, 109, 194, 255).into()
     }
+
+    #[allow(dead_code)]
     pub fn red() -> Color {
         (250, 41, 55, 255).into()
     }
 
+    #[allow(dead_code)]
     pub fn maroon() -> Color {
         (190, 33, 55, 255).into()
     }
 
+    #[allow(dead_code)]
     pub fn green() -> Color {
         (0, 228, 48, 255).into()
     }
 
+    #[allow(dead_code)]
     pub fn lime() -> Color {
         (0, 158, 47, 255).into()
     }
 
+    #[allow(dead_code)]
     pub fn dark_green() -> Color {
         (0, 117, 44, 255).into()
     }
 
+    #[allow(dead_code)]
     pub fn sky_blue() -> Color {
         (102, 191, 255, 255).into()
     }
 
+    #[allow(dead_code)]
     pub fn blue() -> Color {
         (0, 121, 241, 255).into()
     }
 
+    #[allow(dead_code)]
     pub fn purple() -> Color {
         (200, 122, 255, 255).into()
     }
 
+    #[allow(dead_code)]
     pub fn violet() -> Color {
         (135, 60, 190, 255).into()
     }
 
+    #[allow(dead_code)]
     pub fn dark_purple() -> Color {
         (0, 82, 172, 255).into()
     }
 
+    #[allow(dead_code)]
     pub fn beige() -> Color {
         (211, 176, 131, 255).into()
     }
 
+    #[allow(dead_code)]
     pub fn brown() -> Color {
         (127, 106, 79, 255).into()
     }
 
+    #[allow(dead_code)]
     pub fn dark_brown() -> Color {
         (76, 63, 47, 255).into()
     }
 
+    #[allow(dead_code)]
     pub fn white() -> Color {
         (255, 255, 255, 255).into()
     }
 
+    #[allow(dead_code)]
     pub fn black() -> Color {
         (0, 0, 0, 255).into()
     }
 
+    #[allow(dead_code)]
     pub fn magenta() -> Color {
         (255, 0, 255, 255).into()
     }
 
+    #[allow(dead_code)]
     pub fn ray_white() -> Color {
         (245, 245, 245, 255).into()
     }
@@ -120,12 +145,11 @@ impl From<(u8, u8, u8, u8)> for Color {
 
 impl Into<(u8, u8, u8, u8)> for Color {
     fn into(self) -> (u8, u8, u8, u8) {
-        (
-            (self.0.x * 255.0).max(0.0).min(255.0) as u8,
-            (self.0.y * 255.0).max(0.0).min(255.0) as u8,
-            (self.0.z * 255.0).max(0.0).min(255.0) as u8,
-            (self.0.w * 255.0).max(0.0).min(255.0) as u8,
-        )
+        let x = (self.0.x * 255.0f32).max(0.0).min(255.0) as u8;
+        let y = (self.0.y * 255.0f32).max(0.0).min(255.0) as u8;
+        let z = (self.0.z * 255.0f32).max(0.0).min(255.0) as u8;
+        let w = (self.0.w * 255.0f32).max(0.0).min(255.0) as u8;
+        (x, y, z, w)
     }
 }
 
@@ -139,7 +163,6 @@ pub fn build_square_texture<T: Into<Color>>(ctx: &mut Context, width: u16, color
     let color: Color = color.into();
     let mut out: Vec<u8> = Vec::with_capacity((width * width) as usize);
     for _i in 0..(width * width) {
-        let c = color.as_u8();
         for byte in color.as_u8().iter() {
             out.push(*byte);
         }
