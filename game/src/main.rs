cc4e9a1895945dd7bcf2b05e289cd69c15e72ff3
Justin Hurstwright <jryanhurst@gmail.com>
2020-11-27 21:02:41 -0800

    refactor: Split out the stage to a dynamic system

--- game/src/main.rs
+++ game/src/main.rs
@@ -10,13 +10,7 @@ mod shaders;
 mod systems;
 mod utils;
 
-pub struct GameWorld {
-    pub world: hecs::World,
-    pub events: SmallVec<[events::Event; 32]>,
-    pub camera: components::Camera2D,
-}
-
-struct Stage {
+struct GameState {
     direction: components::Direction,
     game_world: GameWorld,
     input: components::Input,
@@ -25,7 +19,7 @@ struct Stage {
     renderer: graphics::MainRenderer,
 }
 
-impl Stage {
+impl GameState {
     pub fn new(ctx: &mut Context) -> Self {
         let renderer = graphics::MainRenderer::new(ctx);
         let mut game_world = GameWorld {
@@ -49,18 +43,18 @@ impl Stage {
             components::Position(Vec2::new(0., -1.)),
         ));
 
-        Stage {
+        GameState {
             direction: components::Direction::Up,
             game_world,
             renderer,
-            move_timer: components::Timer::new(0.75),
+            move_timer: components::Timer::new(0.25),
             input: components::Input::default(),
             food_timer: components::Timer::new(1.5),
         }
     }
 }
 
-impl EventHandler for Stage {
+impl EventHandler for GameState {
     fn resize_event(&mut self, ctx: &mut Context, _width: f32, _height: f32) {
         self.game_world.camera.resize(ctx);
     }
@@ -123,6 +117,152 @@ impl EventHandler for Stage {
         systems::gather_render_cmds(&mut self.game_world, &mut self.renderer.render_commands);
         systems::debug_render_cmds(&mut self.game_world, &mut self.renderer.render_commands);
         self.renderer.draw(ctx);
+    }
+}
+
+
+pub struct GameWorld {
+    pub world: hecs::World,
+    pub events: SmallVec<[events::Event; 32]>,
+    pub camera: components::Camera2D,
+}
+
+struct Stage {
+    stages: Vec<Box<dyn EventHandler>>,
+}
+
+impl Stage {
+    pub fn new(ctx: &mut Context) -> Self {
+        let mut stages = Vec::with_capacity(8);
+        let game_stage = Box::new(GameState::new(ctx));
+        stages.push(game_stage as Box<dyn EventHandler>);
+        Self {
+            stages
+        }
+    }
+}
+
+
+impl EventHandler for Stage {
+    fn resize_event(&mut self, ctx: &mut Context, width: f32, height: f32) {
+        let stage = match self.stages.get_mut(0) {
+            Some(s) => s,
+            None => return,
+        };
+        stage.resize_event(ctx, width, height)
+    }
+
+    fn update(&mut self, ctx: &mut Context) {
+        let stage = match self.stages.get_mut(0) {
+            Some(s) => s,
+            _ => return
+        };
+        stage.update(ctx);
+    }
+
+    fn key_down_event(
+        &mut self,
+        ctx: &mut Context,
+        keycode: KeyCode,
+        keymods: KeyMods,
+        repeat: bool,
+    ) {
+        let stage = match self.stages.get_mut(0) {
+            Some(s) => s,
+            _ => return,
+        };
+        stage.key_down_event(ctx, keycode, keymods, repeat);
+    }
+
+    fn draw(&mut self, ctx: &mut Context) {
+        let stage = match self.stages.get_mut(0) {
+            Some(s) => s,
+            _ => return,
+        };
+        stage.draw(ctx);
+    }
+
+    fn mouse_motion_event(&mut self, ctx: &mut Context, x: f32, y: f32) {
+        let stage = match self.stages.get_mut(0) {
+            Some(s) => s,
+            _ => return,
+        };
+        stage.mouse_motion_event(ctx, x, y);
+    }
+
+    fn mouse_wheel_event(&mut self, ctx: &mut Context, x: f32, y: f32) {
+        let stage = match self.stages.get_mut(0) {
+            Some(s) => s,
+            _ => return,
+        };
+        stage.mouse_wheel_event(ctx, x, y);
+    }
+
+    fn mouse_button_down_event(
+        &mut self,
+        ctx: &mut Context,
+        button: MouseButton,
+        x: f32,
+        y: f32,
+    ) {
+        let stage = match self.stages.get_mut(0) {
+            Some(s) => s,
+            _ => return,
+        };
+        stage.mouse_button_down_event(ctx, button, x, y);
+    }
+
+    fn mouse_button_up_event(
+        &mut self,
+        ctx: &mut Context,
+        button: MouseButton,
+        x: f32,
+        y: f32,
+    ) {
+        let stage = match self.stages.get_mut(0) {
+            Some(s) => s,
+            _ => return,
+        };
+        stage.mouse_button_up_event(ctx, button, x, y);
+    }
+
+    fn char_event(
+        &mut self,
+        ctx: &mut Context,
+        character: char,
+        keymods: KeyMods,
+        repeat: bool,
+    ) {
+        let stage = match self.stages.get_mut(0) {
+            Some(s) => s,
+            _ => return,
+        };
+        stage.char_event(ctx, character, keymods, repeat);
+    }
+
+    fn key_up_event(&mut self, ctx: &mut Context, keycode: KeyCode, keymods: KeyMods) {
+        let stage = match self.stages.get_mut(0) {
+            Some(s) => s,
+            _ => return,
+        };
+        stage.key_up_event(ctx, keycode, keymods);
+
+    }
+
+    fn raw_mouse_motion(&mut self, ctx: &mut Context, dx: f32, dy: f32) {
+        let stage = match self.stages.get_mut(0) {
+            Some(s) => s,
+            _ => return,
+        };
+        stage.raw_mouse_motion(ctx, dx, dy);
+    }
+
+    fn quit_requested_event(&mut self, ctx: &mut Context) {
+        let stage = match self.stages.get_mut(0) {
+            Some(s) => s,
+            _ => return,
+        };
+        stage.quit_requested_event(ctx);
     }
 }
 
