73917f4ac9358f451ec47a957d66eba2bcf5a4ad
Justin Hurstwright <jryanhurst@gmail.com>
2020-11-27 16:01:21 -0800

    refactor: merge in snake_head module

--- game/src/components/mod.rs
+++ game/src/components/mod.rs
@@ -1,14 +1,5 @@
-mod snake_head;
-
-use glam::Vec2;
-use glam::Mat4;
-use glam::Quat;
-use glam::Vec3;
-use miniquad::date;
-use miniquad::Context;
-
-
-pub use self::snake_head::Direction;
+use glam::{Mat4, Quat, Vec2, Vec3};
+use miniquad::{date, Context};
 
 #[derive(Default)]
 pub struct Input {
@@ -95,8 +86,8 @@ pub struct Tail {
     pub ahead: hecs::Entity,
 }
 
-pub struct Position( pub Vec2);
-pub struct Velocity( pub Vec2);
+pub struct Position(pub Vec2);
+pub struct Velocity(pub Vec2);
 
 #[derive(Debug, Copy, Clone)]
 pub enum CollsionKind {
@@ -118,5 +109,57 @@ impl Collision {
         Self {
             kind: CollsionKind::Food,
         }
+    }
+}
+
+#[derive(PartialEq)]
+pub enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
+impl Direction {
+    pub fn velocity(&self) -> Vec2 {
+        match self {
+            Direction::Up => Vec2::new(0., 1.),
+            Direction::Right => Vec2::new(1., 0.),
+            Direction::Down => Vec2::new(0., -1.),
+            Direction::Left => Vec2::new(-1., 0.),
+        }
+    }
+
+    pub fn update(&mut self, input: &crate::components::Input) {
+        if input.go_left {
+            if self == &Direction::Right {
+                return;
+            }
+            *self = Direction::Left;
+        }
+        if input.go_right {
+            if self == &Direction::Left {
+                return;
+            }
+            *self = Direction::Right;
+        }
+        if input.go_down {
+            if self == &Direction::Up {
+                return;
+            }
+            *self = Direction::Down;
+        }
+        if input.go_up {
+            if self == &Direction::Down {
+                return;
+            }
+            *self = Direction::Up;
+        }
+    }
+}
+
+impl Default for Direction {
+    fn default() -> Self {
+        Direction::Up
     }
 }
